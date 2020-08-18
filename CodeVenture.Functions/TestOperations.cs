264e42664d8c02d525ef7695935214d246a6beea
tbodorkos <bodorkos.tibor@gmail.com>
2020-08-18 17:48:20 +0200

    Initialize functions

---
+++ CodeVenture.Functions/TestOperations.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace CodeVenture.Functions
+{
+    public static class TestOperations
+    {
+        [FunctionName("TestOperations")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
